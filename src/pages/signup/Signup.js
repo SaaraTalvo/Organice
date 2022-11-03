669b7013017cd1513379bd025a132e5919a4cb65
Saara Talvo <saara.talvo@vincit.fi>
2022-11-03 11:30:18 +0200

    Add signup form

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,7 +1,54 @@
+import { useState } from "react";
 import "./Signup.css";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [thumbnail, setThumbnail] = useState(null);
+
+  return (
+    <form className="auth-form">
+      <h2>Sign up</h2>
+      <label>
+        <span>Email:</span>
+        <input
+          required
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          //if email changed outside of the form, two way binding
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          required
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <label>
+        <span>Display name:</span>
+        <input
+          required
+          type="text"
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <label>
+        <span>Profile thumbnail:</span>
+        <input
+          required
+          type="file"
+          // onChange={(e) => setDisplayName(e.target.value)}
+        />
+      </label>
+      <button className="btn">Sign up!</button>
+    </form>
+  );
 };
 
 export default Signup;
