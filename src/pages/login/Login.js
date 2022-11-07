7ad4c9c41bbf7e1ded84e363706901148b64f31c
Saara Talvo <saara.talvo@vincit.fi>
2022-11-07 13:25:29 +0200

    Add login form

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,8 +1,49 @@
+import { useState } from "react";
 import "./Login.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <form className="auth-form" onSubmit={handleSubmit}>
+      <h2>Login:</h2>
+      <label>
+        <span>Email:</span>
+        <input
+          required
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          //if email changed outside of the form, two way binding
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          required
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+
+      <button className="btn">login</button>
+
+      {/* {!isPending && <button className="btn">Login</button>}
+      {isPending && (
+        <button className="btn" disabled>
+          Loading...
+        </button>
+      )}
+      {error && <div className="error">{error}</div>} */}
+    </form>
+  );
 };
 
 export default Login;
-<div>Login</div>;
