d951d82aecc70d01ebe459908b7cb365b0a08e97
Saara Talvo <saara.talvo@vincit.fi>
2022-11-07 16:38:29 +0200

    Log user in, track online status

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,13 +1,15 @@
 import { useState } from "react";
+import { useLogin } from "../../hooks/useLogin";
 import "./Login.css";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { login, error, isPending } = useLogin();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email, password);
+    login(email, password);
   };
 
   return (
@@ -32,16 +34,13 @@ const Login = () => {
           value={password}
         />
       </label>
-
-      <button className="btn">login</button>
-
-      {/* {!isPending && <button className="btn">Login</button>}
+      {!isPending && <button className="btn">Login</button>}
       {isPending && (
         <button className="btn" disabled>
           Loading...
         </button>
       )}
-      {error && <div className="error">{error}</div>} */}
+      {error && <div className="error">{error}</div>}
     </form>
   );
 };
