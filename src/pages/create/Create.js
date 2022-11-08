6ee0f6faea8b60771855b6e1df04aa748e5031c6
Saara Talvo <saara.talvo@vincit.fi>
2022-11-08 12:38:48 +0200

    Add Create Project-form

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,7 +1,91 @@
+import { useEffect, useState } from "react";
 import "./Create.css";
+import Select from "react-select";
+import { useCollection } from "../../hooks/useCollection";
+
+const categories = [
+  { value: "development", label: "Development" },
+  { value: "design", label: "Design" },
+  { value: "sales", label: "Sales" },
+  { value: "marketing", label: "Marketing" },
+];
 
 const Create = () => {
-  return <div>Create</div>;
+  const { documents } = useCollection("users");
+  const [users, setUsers] = useState([]);
+
+  //form field values
+  const [name, setName] = useState();
+  const [details, setDetails] = useState();
+  const [dueDate, setDueDate] = useState();
+  const [category, setCategory] = useState();
+  const [assignedUsers, setAssignedUsers] = useState([]);
+
+  //change the users to have the value and label to be shown in the form as select options
+  useEffect(() => {
+    if (documents) {
+      const options = documents.map((user) => {
+        return { value: user, label: user.displayName };
+      });
+      setUsers(options);
+    }
+  }, [documents]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, details, dueDate, category.value, assignedUsers);
+  };
+
+  return (
+    <div className="create-form">
+      <h2 className="page-title">Create a new Project</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Project name:</span>
+          <input
+            type="text"
+            required
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+        </label>
+        <label>
+          <span>Project details:</span>
+          <textarea
+            type="text"
+            required
+            onChange={(e) => setDetails(e.target.value)}
+            value={details}
+          />
+        </label>
+        <label>
+          <span>Project due date:</span>
+          <input
+            type="date"
+            required
+            onChange={(e) => setDueDate(e.target.value)}
+            value={dueDate}
+          />
+        </label>
+        <label>
+          <span>Project category:</span>
+          <Select
+            onChange={(option) => setCategory(option)}
+            options={categories}
+          />
+        </label>
+        <label>
+          <span>Assign project to:</span>
+          <Select
+            onChange={(option) => setAssignedUsers(option)}
+            options={users}
+            isMulti
+          />
+        </label>
+        <button className="btn">Add Project</button>
+      </form>
+    </div>
+  );
 };
 
 export default Create;
