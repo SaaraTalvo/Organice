24a3c47d7c7c22e7f1a5323d8a38cc2399d26324
Saara Talvo <saara.talvo@vincit.fi>
2022-11-16 17:07:19 +0200

    Add comment form on project details page

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import useDocument from "../../hooks/useDocument";
 import "./Project.css";
+import ProjectComments from "./ProjectComments";
 import ProjectSummary from "./ProjectSummary";
 
 const Project = () => {
@@ -17,6 +18,7 @@ const Project = () => {
   return (
     <div className="project-details">
       <ProjectSummary project={document} />
+      <ProjectComments />
     </div>
   );
 };
