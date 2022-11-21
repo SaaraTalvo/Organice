7e80fd4e074229cfa69e38e1187f2976818bb51c
Saara Talvo <saara.talvo@vincit.fi>
2022-11-21 13:42:37 +0200

    Add filter projects functionality

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,16 +1,55 @@
 import "./Dashboard.css";
-
+import { useState } from "react";
 import { useCollection } from "../../hooks/useCollection";
 import ProjectList from "../../components/ProjectList";
+import ProjectFilter from "./ProjectFilter";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 const Dashboard = () => {
+  const { user } = useAuthContext();
   const { documents, error } = useCollection("projects");
+  const [currentFilter, setCurrentFilter] = useState("all");
+
+  const changeFilter = (newFilter) => {
+    setCurrentFilter(newFilter);
+  };
+
+  const filteredProjects = documents
+    ? documents.filter((document) => {
+        switch (currentFilter) {
+          case "all":
+            return true;
+          case "mine":
+            let assignedToMe = false;
+            document.assignedUsersList.forEach((u) => {
+              if (user.uid === u.id) {
+                assignedToMe = true;
+              }
+            });
+            return assignedToMe;
+          case "developent":
+          case "design":
+          case "sales":
+          case "marketing":
+            console.log(document.category, currentFilter);
+            return document.category === currentFilter;
+          default:
+            return true;
+        }
+      })
+    : null;
 
   return (
     <div>
       <h2 className="page-title">Dashboard</h2>
       {error && <p className="error">{error}</p>}
-      {documents && <ProjectList projects={documents} />}
+      {documents && (
+        <ProjectFilter
+          currentFilter={currentFilter}
+          changeFilter={changeFilter}
+        />
+      )}
+      {filteredProjects && <ProjectList projects={filteredProjects} />}
     </div>
   );
 };
